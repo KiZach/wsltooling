8be22c32a36160c0c66016c5677abced8ed3738a
Kim Zachariassen <kza@z4it.com>
2024-06-14 19:02:58 +0200

    AzCLIAdded

--- scripts/install/installAllSoftware.sh
+++ scripts/install/installAllSoftware.sh
@@ -8,6 +8,9 @@ setUserName "$(whoami)"
 
 bash ${DIR_ME}/../config/system/prepareXServer.sh ${USERNAME}
 
+echo -e "\n\nInstalling Azure CLI"
+bash ${DIR_ME}/installAzCli.sh
+
 #echo -e "\n\nInstalling OpenVSCode Server"
 #bash ${DIR_ME}/installOpenVSCodeServer.sh
 

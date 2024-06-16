361d81618ac3db6ab08697652a4274c9fc0ffe9b
Kim Zachariassen <kza@z4it.com>
2024-06-16 14:50:32 +0200

    Update

--- installUbuntuLTS.ps1
+++ installUbuntuLTS.ps1
@@ -15,22 +15,32 @@ if (-Not (Test-Path -Path .\staging\ubuntuLTS.appx)) {
 
 Expand-Archive .\staging\$wslName.zip .\staging\$wslName
 
+# Create distro path if not exists
 if (-Not (Test-Path -Path $wslInstallationPath)) {
     mkdir $wslInstallationPath
 }
+
+# Remove distro is exists
+if ((Get-ChildItem -Path $wslInstallationPath\*.vhdx -force | Measure-Object).Count -ne 0) {
+    $null = wsl --unregister $wslName
+}
+
+# Import new distro
 wsl --import $wslName $wslInstallationPath .\staging\$wslName\install.tar.gz
 
+# Clean up
 Remove-Item -r .\staging\$wslName\
 
 # Update the system
 wsl -d $wslName -u root bash -ic "apt update; apt upgrade -y"
 
-# create your user and add it to sudoers
+# Create your user and add it to sudoers
 wsl -d $wslName -u root bash -ic "./scripts/config/system/createUser.sh $username ubuntu"
 
-# ensure WSL Distro is restarted when first used with user account
+# Ensure WSL Distro is restarted when first used with user account
 wsl -t $wslName
 
+# Install software if selected
 if ($installAllSoftware -ieq $true) {
     wsl -d $wslName -u root bash -ic "./scripts/config/system/sudoNoPasswd.sh $username"
     wsl -d $wslName -u root bash -ic ./scripts/install/installBasePackages.sh
